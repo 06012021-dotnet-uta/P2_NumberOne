b521f7ecf549c7671352500f3863fbf50b6f47fa
Elizabeth N <liznguyen12@gmail.com>
2021-07-18 08:19:50 -0700

    Fixes minor merge errors

--- FindMyPetUI/src/app/app.module.ts
+++ FindMyPetUI/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LoggerModule, NgxLoggerLevel } from "ngx-logger";
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
